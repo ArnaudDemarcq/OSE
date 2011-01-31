959899fb5b565b3fa3159f16ca839327c6a80ba6
Arnaud DEMARCQ <arnaud.demarcq@humanlog.fr>
2011-01-31 13:43:35 +0100

    Added support for OSGI War Bundles

--- OSE/Wizard/src/main/resources/files/startup.bat
+++ OSE/Wizard/src/main/resources/files/startup.bat
@@ -1,8 +1,12 @@
 @ECHO OFF 
 
 set OSGI_IMPL=org.eclipse.osgi-3.6.0.v20100517.jar
+set OSE_HOME=.
 REM set OSGI_IMPL=org.eclipse.osgi_3.4.3.R34x_v20081215-1030.jar
 java   ^
+		-Djetty.home="%OSE_HOME%" ^
+			-Djetty.port=8282 ^
+			-Djetty.logs="%OSE_HOME%/logs" ^
 		-Dcom.sun.management.jmxremote ^
 			-Dcom.sun.management.jmxremote.port=5222 ^
 			-Dcom.sun.management.jmxremote.authenticate=false ^
